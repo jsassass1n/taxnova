7545c016b059152afcd2fd2345cf50ee24c1f91f
Dmitriy.Cherniy <Dmitriy.Cherniy@elnk.com>
2025-12-10 19:45:10 +0400

    lAST COMMIT

--- app/components/main/contact-us.tsx
+++ app/components/main/contact-us.tsx
@@ -165,7 +165,7 @@ export default function ContactUs() {
             )}
           </div>
         </form>
-        <div className="space-y-8 text-center md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
+        <div className="mx-auto grid max-w-md grid-cols-2 gap-0 text-center">
           <div>
             <div className="mx-auto mb-4 flex h-10 w-10 items-center justify-center rounded-lg bg-gray-100 lg:h-16 lg:w-16 dark:bg-gray-800">
               <svg
@@ -178,16 +178,12 @@ export default function ContactUs() {
                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
               </svg>
             </div>
-            <p className="mb-2 text-xl font-bold dark:text-white">Email us:</p>
-            <p className="mb-3 text-gray-500 dark:text-gray-400">
-              Email us for general queries, including marketing and partnership
-              opportunities.
-            </p>
+            <p className="mb-2 text-xl font-bold dark:text-white">Email</p>
             <a
               href="mailto:abc@example.com"
               className="text-primary-600 dark:text-primary-500 font-semibold hover:underline"
             >
-              hello@flowbite.com
+              info@taxnova.com
             </a>
           </div>
           <div>
@@ -201,41 +197,17 @@ export default function ContactUs() {
                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
               </svg>
             </div>
-            <p className="mb-2 text-xl font-bold dark:text-white">Call us:</p>
-            <p className="mb-3 text-gray-500 dark:text-gray-400">
-              Call us to speak to a member of our team. We are always happy to
-              help.
+            <p className="mb-2 text-xl font-bold dark:text-white">
+              Əlaqə nömrəsi
             </p>
             <span className="text-primary-600 dark:text-primary-500 font-semibold">
-              +1 (646) 786-5060
-            </span>
-          </div>
-          <div>
-            <div className="mx-auto mb-4 flex h-10 w-10 items-center justify-center rounded-lg bg-gray-100 lg:h-16 lg:w-16 dark:bg-gray-800">
-              <svg
-                className="h-5 w-5 text-gray-600 lg:h-8 lg:w-8 dark:text-gray-500"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+              <a
+                className="text-primary-600 dark:text-primary-500 font-semibold"
+                href="tel:+994552169018"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-2 0c0 .993-.241 1.929-.668 2.754l-1.524-1.525a3.997 3.997 0 00.078-2.183l1.562-1.562C15.802 8.249 16 9.1 16 10zm-5.165 3.913l1.58 1.58A5.98 5.98 0 0110 16a5.976 5.976 0 01-2.516-.552l1.562-1.562a4.006 4.006 0 001.789.027zm-4.677-2.796a4.002 4.002 0 01-.041-2.08l-.08.08-1.53-1.533A5.98 5.98 0 004 10c0 .954.223 1.856.619 2.657l1.54-1.54zm1.088-6.45A5.974 5.974 0 0110 4c.954 0 1.856.223 2.657.619l-1.54 1.54a4.002 4.002 0 00-2.346.033L7.246 4.668zM12 10a2 2 0 11-4 0 2 2 0 014 0z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </div>
-            <p className="mb-2 text-xl font-bold dark:text-white">Support</p>
-            <p className="mb-3 text-gray-500 dark:text-gray-400">
-              Email us for general queries, including marketing and partnership
-              opportunities.
-            </p>
-            <a
-              href="#"
-              className="text-primary-600 border-primary-600 hover:bg-primary-600 focus:ring-primary-300 dark:border-primary-500 dark:text-primary-500 dark:hover:bg-primary-600 dark:focus:ring-primary-800 inline-flex rounded-lg border px-4 py-2 text-center text-sm font-medium hover:text-white focus:ring-4 focus:outline-none dark:hover:text-white"
-            >
-              Support center
-            </a>
+                +994 55 216 90 18
+              </a>
+            </span>
           </div>
         </div>
       </div>
